a0359477f46213c61669bbc5601d4ee5df7fa981
Karol-Sun <ksun26@jh.edu>
2023-12-12 19:47:29 -0500

    commented on main, given a light rotation example

--- Raytracing_main.py
+++ Raytracing_main.py
@@ -5,23 +5,38 @@ from PIL import Image
 
 def normalize(x):
     '''
-        Vector normalization.
+    Vector normalization.
 
-        **Parameters**
+    **Parameters**
 
-            x: * array *
-                vector to be normalized
+        x: * array *
+            vector to be normalized
 
-        **Returns**
+    **Returns**
 
-            color_map: *dict, int, tuple*
-                A dictionary that will correlate the integer key to
-                a color.
-        '''
+        normalized x
+    '''
     return x / np.linalg.norm(x)   # normalization
 
 
 def generate_points_on_circle(radius, y_set, num_points):
+    '''
+    generate points on a circle centered (x=0, z=0).
+
+    **Parameters**
+
+        radius: * float *
+            radius of the circle
+        y_set: * float *
+            given y value of points
+        num_points: * int *
+            number of points generated
+
+    **Returns**
+
+        coordinates: * list *
+            a list of points generated
+    '''
     # Generate equally spaced angles
     theta_values = np.linspace(0, 2 * np.pi, num_points, endpoint=False)
 
@@ -37,6 +52,22 @@ def generate_points_on_circle(radius, y_set, num_points):
 
 
 def generate_pic(O, Q, k):
+    '''
+    generate picture given camera position and direction.
+
+    **Parameters**
+
+        O: * array *
+            camera position
+        Q: * array *
+            camera direction
+        k: * int *
+            index of picture
+
+    **Returns**
+
+        None
+    '''
     dir = np.copy(Q)
     img = np.zeros((h, w, 3))
     r = float(w) / h
@@ -54,12 +85,41 @@ def generate_pic(O, Q, k):
 
 
 class Ray:
+    '''
+    This class handles the position and direction of the ray we trace
+
+    **Parameters**
+    origin: * array *
+        ray startpoint (camera position)
+    direction: * array *
+        endpoint of the current ray
+    '''
     def __init__(self, origin, direction):
         self.origin = origin
         self.direction = direction / np.linalg.norm(direction)
 
 
 class Object:
+    '''
+    This class handles the objects construct the virtual world
+
+    **Parameters**
+    position: * array *
+        object position (center of sphere or a point on the plane)
+    color: * array *
+        RGB color of the object
+    reflection: * float *
+        Represents the amount of light that is reflected specularly (mirror-like reflection) from the surface.
+    diffuse: * float *
+        Represents the amount of light that is diffusely reflected from the surface. Diffuse reflection occurs
+        when light is scattered in various directions.
+    specular_c: * float *
+        Represents the intensity of the specular reflection. It controls how "shiny" or "specular" the material
+        appears. Lower values result in a broader and less intense specular highlight.
+    specular_k: * float *
+        Represents the shininess or smoothness of the material. Higher values result in a smaller and more concentrated
+        specular highlight. This value is often referred to as the specular exponent or shininess coefficient.
+    '''
     def __init__(self, position, color, reflection=0.85, diffuse=1.0, specular_c=0.6, specular_k=50):
         self.position = np.array(position)
         self.color = np.array(color)
@@ -74,16 +134,36 @@ class Object:
     def get_normal(self, point):
         pass
 
-    def get_color(self, point):
+    def get_color(self, point):  # return color of the object
         return self.color
 
 
 class Sphere(Object):
+    '''
+    This class handles further features of sphere on the basis of Object
+
+    **Parameters**
+    radius: * float *
+        radius of the sphere
+    others: same as "Object" class
+    '''
     def __init__(self, position, radius, color, **kwargs):
         super().__init__(position, color, **kwargs)
         self.radius = np.array(radius)
 
     def intersect(self, ray):
+        '''
+        Intersection test
+
+        **Parameters**
+
+            ray: * object *
+                current ray to be traced
+
+        **Returns**
+
+            distance between intersection point and ray startpoint
+        '''
         OC = self.position - ray.origin
         if np.linalg.norm(OC) < self.radius or np.dot(OC, ray.direction) < 0:
             return np.inf
@@ -97,6 +177,14 @@ class Sphere(Object):
 
 
 class Plane(Object):
+    '''
+    This class handles further features of plane on the basis of Object
+
+    **Parameters**
+    normal: * array *
+        normal vector of the plane
+    others: same as "Object" class
+    '''
     def __init__(self, position, normal, width=1.0, height=1.0, texture_file=None, reflection=0.15, diffuse=0.75,
                  specular_c=0.3, specular_k=50, **kwargs):
         super().__init__(position, None, reflection=reflection, diffuse=diffuse, specular_c=specular_c,
@@ -135,6 +223,19 @@ class Plane(Object):
 
 
 class Scene:
+    '''
+    This class handles the scene (virtual world)
+
+    **Parameters**
+    objects: * list *
+        a list of objects created
+    light_point: * array *
+        position of point light source
+    light_color: * array *
+        RGB color of point light source
+    ambient: * float *
+        intensity of ambient light
+    '''
     def __init__(self, objects, light_point, light_color, ambient):
         self.objects = objects
         self.light_point = np.array(light_point)
@@ -187,7 +288,7 @@ if __name__ == "__main__":
     # scene_objects = [sphere, plane]
 
 
-    ##### light rotation #####
+    ##### 1 light rotation #####
     # light = generate_points_on_circle(radius=10, y_set=5, num_points=500)
     # O = np.array([0., 0.35, -1.])  # camera position
     # Q = np.array([0., 0., 0.])  # camera direction
@@ -198,7 +299,7 @@ if __name__ == "__main__":
     #     ray_scene = Scene(scene_objects, light_point, light_color, ambient)
     #     generate_pic(O, Q, k)
 
-    ##### camera movement #####
+    ##### 2 camera movement #####
     # ray_scene = Scene(scene_objects, light_point, light_color, ambient)
     # w, h = 800, 600
     #
@@ -209,7 +310,7 @@ if __name__ == "__main__":
     #     O[0] += 0.2
     #     Q[0] += 0.2
 
-    ##### camera rotation #####
+    ##### 3 camera rotation #####
     ray_scene = Scene(scene_objects, light_point, light_color, ambient)
     w, h = 800, 600
 
@@ -218,9 +319,10 @@ if __name__ == "__main__":
     center = np.array([-1., 0., 1.75])  # camera center
     O = np.array([-1, 0.35, center[2] + r1])  # camera position
     Q = np.array([-1, 0.35, center[2] + r2])  # camera direction
-    for k in range(20):
+    num = 1000  # number of picture to be rendered
+    for k in range(num):
         generate_pic(O, Q, k)
-        O[0] = center[0] + np.sin((k + 1) * np.pi / 10) * r1
-        O[2] = center[2] + np.cos((k + 1) * np.pi / 10) * r1
-        Q[0] = center[0] + np.sin((k + 1) * np.pi / 10) * r2
-        Q[2] = center[2] + np.cos((k + 1) * np.pi / 10) * r2
+        O[0] = center[0] + np.sin((k + 1) * 2 * np.pi / num) * r1
+        O[2] = center[2] + np.cos((k + 1) * 2 * np.pi / num) * r1
+        Q[0] = center[0] + np.sin((k + 1) * 2 * np.pi / num) * r2
+        Q[2] = center[2] + np.cos((k + 1) * 2 * np.pi / num) * r2
